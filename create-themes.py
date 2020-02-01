0769932e60b79634556e8be8094a414a1135bd95
Bas Nijholt <basnijholt@gmail.com>
2020-02-01 18:54:30 +0100

    generate active-state-color themes

--- create-themes.py
+++ create-themes.py
@@ -23,19 +23,29 @@ with open(f"ios-themes.yaml", "w") as f:
 for background in Path("backgrounds").glob("homekit-bg-*.jpg"):
     color = background.stem.split("homekit-bg-")[-1]
     for which in ["light", "dark"]:
-        settings = {k: parse(v[which]) for k, v in all_settings.items()}
+        for state_icon_yellow in [False, True]:
+            settings = {k: parse(v[which]) for k, v in all_settings.items()}
 
-        with open("template.jinja2") as f:
-            template = jinja2.Template("".join(f.readlines()))
+            if state_icon_yellow:
+                settings["state_icon_active_color"] = "rgba(255, 214, 10, 1)"
+                suffix = "-alternative"
+            else:
+                suffix = ""
 
-        with background.open("rb") as f:
-            background_base64 = base64.b64encode(f.read()).decode()
+            with open("template.jinja2") as f:
+                template = jinja2.Template("".join(f.readlines()))
 
-        result = template.render(
-            **settings, which=which, background_base64=background_base64, color=color
-        )
-        with open(f"themes/ios-{which}-mode-{color}.yaml", "w") as f:
-            f.write("---\n" + result + "\n")
+            with background.open("rb") as f:
+                background_base64 = base64.b64encode(f.read()).decode()
+            result = template.render(
+                **settings,
+                which=which,
+                background_base64=background_base64,
+                color=color,
+                suffix=suffix,
+            )
+            with open(f"themes/ios-{which}-mode-{color}{suffix}.yaml", "w") as f:
+                f.write("---\n" + result + "\n")
 
-        with open(f"ios-themes.yaml", "a") as f:
-            f.write("\n" + result + "\n")
+            with open(f"ios-themes.yaml", "a") as f:
+                f.write("\n" + result + "\n")
